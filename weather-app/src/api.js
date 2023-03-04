fcc8f53898b2a0f7fb8d90ec7606312fb7cce192
Vaibhav <42316158+VaibhavBomle@users.noreply.github.com>
2023-03-04 16:05:24 +0530

    Implemented base project ,fetching geo location API

---
+++ weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7a0b47c024msh190ae9c27455e27p100466jsna71eba59e502',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
